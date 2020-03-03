e725f93c13419678a362bf6f5c6ffdafee97b560
Summer <su943515688@gmail.com>
2020-03-03 21:41:12 +0800

    [+] 根据服务器读取配置文件

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log-agent/config"
 	"log-agent/etcd"
 	"log-agent/kafka"
 	"log-agent/taillog"
+	"log-agent/utils"
 	"sync"
 	"time"
 )
@@ -12,10 +14,13 @@ import (
 func main() {
 	// 初始化etcd
 	etcd.Init(config.Config.Endpoints, time.Duration(config.Config.Timeout)*time.Second)
-	logConf := etcd.GetLogConf(config.Config.Key)
+	logConfKey := config.Config.Key
+	// 获取每个服务器对应的配置文件
+	logConfKey = fmt.Sprintf(logConfKey, utils.GetOutboundIP())
+	logConf := etcd.GetLogConf(logConfKey)
 	// 初始化kafka
 	kafka.Init(config.Config.Address, config.Config.MaxSize)
-	watchLogConfChan := etcd.WatchLogConf(config.Config.Key)
+	watchLogConfChan := etcd.WatchLogConf(logConfKey)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	taillog.Init(logConf, watchLogConfChan)
